d2841fd2f4dae123a9095155ca65e29ffea483fd
PerkinsZhu <zhupingjing@bersonfintech.com>
2022-11-16 13:20:38 +0800

    调试加载配置文件

--- src/mod/applications/mod_helloworld/mod_helloworld.c
+++ src/mod/applications/mod_helloworld/mod_helloworld.c
@@ -16,21 +16,28 @@ static void event_handler(switch_event_t* event)
 
 }
 
-static void load_config()
+// 加载配置文件开始
+static struct
 {
-	switch_xml_t x_queues, x_queue, cfg, x_agents, x_agent, x_tiers;
-	switch_xml_t xml = NULL;
-	switch_event_t* event = NULL;
-	switch_event_t* params = NULL;
+	char* hostname;      /* 数据库服务器地址 */
+	char* dbname;      /* 数据库实例名 */
+	int	  port;      /* 数据库端口 */
+	char* user;      /* 数据库用户 */
+	char* password;      /* 数据库密码 */
+} db_globals_t;
 
-	switch_event_create(&params, SWITCH_EVENT_REQUEST_PARAMS);
-	switch_assert(params);
-	switch_event_add_header_string(params, SWITCH_STACK_BOTTOM, "CC-Queue", queue_name);
 
-	if (!(xml = switch_xml_open_cfg(global_cf, &cfg, params))) {
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Open of %s failed\n", global_cf);
-		goto end;
-	}
+
+static switch_xml_config_item_t instructions[] = {
+	/* parameter name        type                 reloadable   pointer                         default value     options structure */
+	SWITCH_CONFIG_ITEM_STRING_STRDUP("db-hostname", CONFIG_RELOAD, &db_globals_t.hostname, NULL, "localhost", "Hostname for db server"), /* 字符串用这个 */
+	SWITCH_CONFIG_ITEM_STRING_STRDUP("db-name", CONFIG_RELOAD, &db_globals_t.dbname, NULL, "xcroute", "dbname for db server"),
+	SWITCH_CONFIG_ITEM_STRING_STRDUP("db-user", CONFIG_RELOAD, &db_globals_t.user, NULL, "postgres", "user for db server"),
+	SWITCH_CONFIG_ITEM_STRING_STRDUP("db-password", CONFIG_RELOAD, &db_globals_t.password, NULL, "abc123", "password for db server"),
+	SWITCH_CONFIG_ITEM("db-port", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &db_globals_t.port, (void*)5432, NULL,NULL, NULL), /* 数字用这个 */
+	SWITCH_CONFIG_ITEM_END()
+};
+// 加载配置文件结束
 
 
 }
@@ -43,6 +50,22 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_helloworld_load)
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Couldn't bind!\n");
 		return SWITCH_STATUS_GENERR;
 	}
+
+
+
+	//load模块里写下
+	//注意这里的 helloworld.conf 要和 xml文件里面的 <configuration name="helloworld.conf"/> 保持一致
+	res = switch_xml_config_parse_module_settings("helloworld.conf", SWITCH_FALSE, instructions);
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "res %d !\n", res);
+	if (res != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "load configure failed!\n");
+		return SWITCH_STATUS_FALSE;
+	}
+	//输出加载的 配置文件参数
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "hostname: %s \nport: %d", db_globals_t.hostname, db_globals_t.port);
+
+
+
 	return SWITCH_STATUS_SUCCESS;
 }
 
